4b510e8b8563a44fa4cd27d9406e2ab7dd551d3a
Mickaël Floc’hlay <mickael@flochlay.com>
2017-02-20 11:22:24 +0100

    Add Pods to the repo

---
+++ Example/Pods/Target Support Files/Pods-Martinet_Example/Pods-Martinet_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Martinet_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Martinet_Example
+@end
---
+++ Rakefile
@@ -0,0 +1,4 @@
+desc 'Install pods'
+task :pod_install do |t|
+  sh "bundle exec pod install --project-directory=Example/"
+end
